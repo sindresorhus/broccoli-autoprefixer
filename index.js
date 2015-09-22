ef1c1ba6f6773a782024a75547f8bed9aacb4d74
Chad Hietala <chadhietala@gmail.com>
2015-09-22 14:16:40 -0700

    Use Persistent Filter
    The persistent filter allows for both in between rebuild catching and
    also warm boot as we persist the last state to the system tmp directory.
    
    https://github.com/stefanpenner/broccoli-persistent-filter

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
-var Filter = require('broccoli-filter');
+var Filter = require('broccoli-persistent-filter');
 var objectAssign = require('object-assign');
 var postcss = require('postcss');
 var autoprefixer = require('autoprefixer');
+var crypto = require('crypto');
+var stringify = require('json-stable-stringify');
 
 function AutoprefixerFilter(inputTree, options) {
 	if (!(this instanceof AutoprefixerFilter)) {
@@ -13,6 +15,7 @@ function AutoprefixerFilter(inputTree, options) {
 
 	this.inputTree = inputTree;
 	this.options = options || {};
+	this.persist = true;
 }
 
 AutoprefixerFilter.prototype = Object.create(Filter.prototype);
@@ -20,6 +23,21 @@ AutoprefixerFilter.prototype.constructor = AutoprefixerFilter;
 
 AutoprefixerFilter.prototype.extensions = ['css'];
 AutoprefixerFilter.prototype.targetExtension = 'css';
+
+AutoprefixerFilter.prototype.baseDir = function () {
+	return __dirname;
+};
+
+AutoprefixerFilter.prototype.optionsHash = function () {
+	if (!this._optionsHash) {
+		this._optionsHash = crypto.createHash('md5').update(stringify(this.options), 'utf8').digest('hex');
+	}
+	return this._optionsHash;
+};
+
+AutoprefixerFilter.prototype.cacheKeyProcessString = function (string, relativePath) {
+	return this.optionsHash() + Filter.prototype.cacheKeyProcessString.call(this, string, relativePath);
+};
 
 AutoprefixerFilter.prototype.processString = function (str, relativePath) {
 	var opts = objectAssign({
