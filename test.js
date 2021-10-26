424d2a20df08654511e124c508c8199a8ea6caf2
Alon Bukai <alonzorz@gmail.com>
2021-10-26 21:14:06 +0300

    Upgrade dependencies (#23)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
-import test from 'ava';
-import del from 'del';
+const fs = require('fs');
+const test = require('ava');
+const del = require('del');
 
 test.afterEach(() => {
 	del.sync('temp');
